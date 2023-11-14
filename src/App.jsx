598be2c711b7cd13a9e7dea519dc5d92d74fb8cc
Derki26 <Derki26.github.com>
2023-11-13 19:00:14 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,108 @@
+import "./App.css";
+import {useState} from "react";
+import { Light } from "./Light";
+import {useEffect} from "react";
+
+
+
+export function App() {
+
+  const [activeLight, setActiveLight] = useState("red");
+  
+  //change color purple
+  const [isPurple, setIsPurple] = useState(false);
+
+  //announcement
+  const [announcement, setAnnouncement] = useState("");
+  
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      switch (activeLight) {
+        case "red":
+          setActiveLight("yellow");
+          break;
+        case "yellow":
+          setActiveLight("green");
+          break;
+        case "green":
+          if (isPurple) {
+            setActiveLight("purple");
+          } else {
+            setActiveLight("red");
+          }
+          break;
+        case "purple":
+          setActiveLight("red");
+          break;
+        default:
+          break;
+      }
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [activeLight, isPurple]);
+
+
+  const handleButtonClick = () => {
+    
+    switch (activeLight) {
+      case "red":
+        setActiveLight("yellow");
+        break;
+      case "yellow":
+        setActiveLight("green");
+        break;
+      case "green":
+        if(isPurple){
+					setActiveLight("purple") 
+				}else{
+					setActiveLight("red");
+				}	
+				break;
+			case "purple":
+				setActiveLight("red");
+				break;
+    }
+  };
+  
+  
+
+  const handlePurpleButtonClick = () => {
+    setIsPurple(true);
+    setAnnouncement("The color purple Added to the Trafic Light!");
+ 
+  };
+
+  return (
+    <>
+      <div className="stick" />
+      <div className="trafficLight">
+        <Light color="red" opacity={activeLight === "red" ? 1 : 0.1} />
+        <Light color="yellow" opacity={activeLight === "yellow" ? 1 : 0.1} />
+        <Light color="green" opacity={activeLight === "green" ? 1 : 0.1} />
+
+        {isPurple && (<Light color="purple" opacity={activeLight === "purple" ? 1 : 0.1} />)}
+
+      </div>
+      <h1 style={{ width: "fit-content", margin: "auto", color:"white"}}>{activeLight}</h1>
+      
+      <div className="button-container">
+        
+          <button onClick={handleButtonClick} className="button-div secondary margin-1">
+            Alternate Color
+           </button>
+           <div>
+              <button onClick={handlePurpleButtonClick} className="button-div secondary margin-1">
+                 Add Purple
+                
+              </button>
+              
+          </div>
+          {announcement && <p>{announcement}</p>}
+      </div>
+    </>
+  );
+}
+
+export default App;
